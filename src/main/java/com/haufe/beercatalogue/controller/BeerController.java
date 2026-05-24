1440e792aabb74dc72367da72ad5ec5072d3de71
Nadya <subs4mail@gmail.com>
2026-05-24 14:19:07 +0200

    chore: centralize API versioning in configuration

--- src/main/java/com/haufe/beercatalogue/controller/BeerController.java
+++ src/main/java/com/haufe/beercatalogue/controller/BeerController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/beers")
+@RequestMapping("${api.prefix}/beers")
 @RequiredArgsConstructor
 public class BeerController {
 
--- src/main/java/com/haufe/beercatalogue/controller/ManufacturerController.java
+++ src/main/java/com/haufe/beercatalogue/controller/ManufacturerController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1/manufacturers")
+@RequestMapping("${api.prefix}/manufacturers")
 @RequiredArgsConstructor
 public class ManufacturerController {
 
