a477f734b14d7da2f9878aeb1667621a68f335ae
Nadya <subs4mail@gmail.com>
2026-05-26 11:24:26 +0200

    refactor: reduce cognitive complexity of beer specification

--- src/main/java/com/haufe/beercatalogue/repository/specification/BeerSpecification.java
+++ src/main/java/com/haufe/beercatalogue/repository/specification/BeerSpecification.java
@@ -2,36 +2,57 @@ package com.haufe.beercatalogue.repository.specification;
 
 import com.haufe.beercatalogue.dto.BeerSearchCriteria;
 import com.haufe.beercatalogue.model.Beer;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BeerSpecification {
+
+    private BeerSpecification() {
+    }
+
     public static Specification<Beer> filterByCriteria(BeerSearchCriteria criteria) {
         return (root, query, cb) -> {
-            List<Predicate> predicates = new ArrayList<>();
-
-            if (criteria != null) {
-                if (criteria.name() != null && !criteria.name().isBlank()) {
-                    predicates.add(cb.like(cb.lower(root.get("name")), "%" + criteria.name().toLowerCase() + "%"));
-                }
-                if (criteria.type() != null) {
-                    predicates.add(cb.equal(root.get("type"), criteria.type()));
-                }
-                if (criteria.minAbv() != null) {
-                    predicates.add(cb.greaterThanOrEqualTo(root.get("abv"), criteria.minAbv()));
-                }
-                if (criteria.maxAbv() != null) {
-                    predicates.add(cb.lessThanOrEqualTo(root.get("abv"), criteria.maxAbv()));
-                }
-                if (criteria.manufacturerId() != null) {
-                    predicates.add(cb.equal(root.get("manufacturer").get("id"), criteria.manufacturerId()));
-                }
+            if (criteria == null) {
+                return cb.conjunction();
             }
+
+            List<Predicate> predicates = new ArrayList<>();
+            addNameFilter(predicates, root, cb, criteria.name());
+            addEquals(predicates, cb, root.get("type"), criteria.type());
+            addMinAbv(predicates, root, cb, criteria.minAbv());
+            addMaxAbv(predicates, root, cb, criteria.maxAbv());
+            addEquals(predicates, cb, root.get("manufacturer").get("id"), criteria.manufacturerId());
 
             return cb.and(predicates.toArray(new Predicate[0]));
         };
+    }
+
+    private static void addNameFilter(List<Predicate> predicates, Root<Beer> root, CriteriaBuilder cb, String name) {
+        if (name != null && !name.isBlank()) {
+            predicates.add(cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%"));
+        }
+    }
+
+    private static void addMinAbv(List<Predicate> predicates, Root<Beer> root, CriteriaBuilder cb, Double minAbv) {
+        if (minAbv != null) {
+            predicates.add(cb.greaterThanOrEqualTo(root.get("abv"), minAbv));
+        }
+    }
+
+    private static void addMaxAbv(List<Predicate> predicates, Root<Beer> root, CriteriaBuilder cb, Double maxAbv) {
+        if (maxAbv != null) {
+            predicates.add(cb.lessThanOrEqualTo(root.get("abv"), maxAbv));
+        }
+    }
+
+    private static void addEquals(List<Predicate> predicates, CriteriaBuilder cb, jakarta.persistence.criteria.Path<?> path, Object value) {
+        if (value != null) {
+            predicates.add(cb.equal(path, value));
+        }
     }
 }
